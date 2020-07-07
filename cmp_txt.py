1afc5b510e4cbbd3bb89b3d03be99aeacf414d59
twhitbourn <twhitbourn@mcgrathnicol.com>
2020-07-07 11:44:42 +1000

    20200707_2

--- cmp_txt.py
+++ cmp_txt.py
@@ -1,6 +1,6 @@
 import hashlib
 import multiprocessing as mp
-import python-Levenshtein
+import Levenshtein
 
 result_list = []
 
@@ -8,24 +8,55 @@ def log_result(result):
     result_list.append(result)
 
 def work(arr):
-    result = 0;
+    flat_result = 0.0
+    letter_result = 0.0
     try:  
-        result = distnace(arr[0],arr[1])/len(arr[0])
+        flat_result = internal_score(arr[0],arr[1])
+        letter_result = internal_score(letter_string(arr[0]),letter_string(arr[1]))
+
+        
+        #Levenshtein.distance(arr[0],arr[1])/len(arr[0])
     except:
         pass
     finally:
-        return [arr[0],arr[1],result]
+        print(flat_result)
+        print(letter_result)
+        if flat_result == 0.0 and letter_result == 0.0:
+            return [arr[0],arr[1],'0.0']
+        if flat_result == 1.0 and letter_result == 1.0:
+            return [arr[0],arr[1],'1.0']
+        return [arr[0],arr[1],"{:.3f}".format((flat_result * 0.2) + (letter_result * 0.8))]
+        #return [arr[0],arr[1],"{:.3f}".format(result)]
+
+def letter_string(s1):
+    s1 = sorted(s1, key=str.lower)
+    strOut = ""
+    print(s1)
+    for s in s1:
+        if s.isdigit() or s.isalpha():
+            strOut += s
+    print(strOut)
+    return strOut
+
+def internal_score(s1,s2):
+    max_len = max(len(s1), len(s2))
+    if max_len == 0:
+        return 0.0
+    dist = Levenshtein.distance(s1,s2)
+    return 1 - (dist / max_len)
+    
 
 def apply_async_with_callback():
     pool = mp.Pool()
     strings = []
+    letter_string("free-made bakes")
     with open('input.txt','r') as f:
         for i, line in enumerate(f):
             arr = line.strip().split('\t')
-            if len(arr)=2:
-                string += [arr]
+            if len(arr)==2:
+                strings += [arr]
                 #print(arr)
-                pool.apply_async(work, args = (arr, ), callback = log_results)
+                pool.apply_async(work, args = (arr, ), callback = log_result)
     pool.close()
     pool.join()
     #print(result_list)
